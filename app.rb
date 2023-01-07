04e06a49032669e767b217069e95aeb57d072485
Ryan Horn <rj.horn@gmail.com>
2023-01-07 12:04:49 -0700

    commit tree 1968f0ad295587d223d46c68ee89d5d580136201

--- app.rb
+++ app.rb
@@ -31,3 +31,52 @@ puts add(1,3) + add(2,2)
 #lets merge this ryry branch into our main
 
 #lets try to get branch merge and delete after merge
+
+
+puts "Hello world!"
+
+p [1,2,3]
+
+
+
+# pp [{ id: 1, hello: "World" }, 
+#     { id: 2, hello: "Ruby" }, 
+#     { id: 3, hello: "Moon" }, 
+#     { id: 4, hello: "Learner" }]
+
+
+puts "Hello world!"
+
+# p [1,2,3]
+# puts "Hello World!"
+# print "Pass this test, please."
+# p [1,2,3]
+
+# def add
+    
+# end
+# # 1 + 1
+# puts add + add
+
+def add num1, num2
+    puts num1
+    puts num2
+    num1 + num2
+end
+
+puts add(1,3) + add(2,2)
+
+
+#I'm going to try to manage a new branch
+
+#Some more changes to this branch
+#lets merge this ryry branch into our main
+
+#lets try to get branch merge and delete after merge
+
+
+puts "Hello world!"
+
+p [1,2,3]
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
